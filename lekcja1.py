4e5e7a341fe40b5578cd995fe2d4eee00a82912a
Pawel <poczta130@gmail.com>
2020-03-06 18:20:20 +0100

    Create lekcja1.py

---
+++ lekcja1.py
@@ -0,0 +1,23 @@
+# if-else-elif 
+
+a,b,c = 1,2,1
+
+delta=b**2 -4*a*c
+
+if delta>0:
+  print("2 rozwiazania w zbiorze liczb rzeczywistych")
+elif delta == 0:
+  print("1 rozwiazanie w zbiorze liczb rzeczywistych")
+else:
+  print("Brak rozwiazan")
+  
+  
+  
+
+
+
+
+
+
+
+
