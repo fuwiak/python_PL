b702273fde7a580a0d19ea9c633fdfa8f2a0b2b2
Pawel <poczta130@gmail.com>
2020-03-06 18:37:49 +0100

    Update lekcja2.py

--- lekcja2.py
+++ lekcja2.py
@@ -7,3 +7,14 @@ lista = [1,2,3,8]
 for liczba in lista:
   print(liczba)
   
+
+#wypisane tylko parzystych elementow listy
+
+for liczba in lista:
+  if liczba%2==0:
+    print(liczba)
+    
+#wypisanie elementow listy wraz z jej indexami
+
+for ind, num in enumerate(lista):
+  print(ind, num)
