db3a4b14c0fb4c718ab18b49d6ca05a759e48540
Pawel <poczta130@gmail.com>
2020-06-04 21:04:03 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,5 +1,18 @@
+potega3=[]
+for i in range(1, 100000):
+  p=3**i
+  if p <100000 and p not in potega3:
+    potega3.append(p)
+  
+ 
 
-
+def silnia(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return silnia(n-1)*n
+  
+#read the file
 data = open("file_name.txt", "r")
 data = data.read()
 data = data.split("\n")
@@ -7,23 +20,10 @@ data = [x for x in data if x!='']
 
 data = [int(x) for x in data]
 
-
-potega3=[]
-for i in range(1, 100000):
-  p=3**i
-  if p <100000 and p not in potega3:
-    potega3.append(p)
-  
-  
 for liczba in data:
   if liczba in potega3:
     print(liczba)
-
-def silnia(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return silnia(n-1)*n
+  
   
 temp_sum=[]
 for liczba in data:
